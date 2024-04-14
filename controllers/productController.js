644e08a65fcac1ac60e09e641fd513ae4bcb7175
elliSzAt <trongellisATTT@gmail.com>
2024-04-14 19:39:12 +0700

    fix_product

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,18 +3,26 @@ import productService from "../services/productService";
 const getProduct = async (req, res) => {
   const query = req.query;
   if (query.id) {
-    try {
-      const product = await productService.getProductById(parseInt(query.id));
-      query["product"] = product;
-      // console.log(product);
-      res.render("shop-product-right", req.query);
-    } catch (err) {
-      console.log(err);
+    const productId = parseInt(query.id);
+    if (!isNaN(productId)) {
+      try {
+        const product = await productService.getProductById(productId);
+        const sanitizedProduct = sanitizeData(product);
+        res.render("shop-product-right", { product: sanitizedProduct });
+      } catch (err) {
+        console.log(err);
+        res.render("404");
+      }
+    } else {
       res.render("404");
     }
   } else {
     res.render("404");
   }
 };
+
+function sanitizeData(data) {
+  return data;
+}
 
 export default { getProduct };
